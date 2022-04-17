36adbf53bdad8602b60f43a42ce4db3de1de555e
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-04-17 08:48:24 +0300

    router links added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './body/body.component';
 import { FormComponent } from './form/form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
-   { path: 'Home' ,component: BodyComponent },
+  { path: 'Home' ,component: BodyComponent },
   { path: 'login' ,component: FormComponent },
   { path: 'logout', component: BodyComponent },
-  { path: '**', component: NotFoundComponent },
+  // { path: '**', component: NotFoundComponent },
 ];
 
 
